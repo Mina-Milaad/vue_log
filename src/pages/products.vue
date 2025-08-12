fd1c9ce083641f5d1a351da79347a596d4316cc1
Mina-Milaad <minaamilaad4@gmail.com>
2025-08-12 11:20:43 +0300

    commit

--- src/pages/products.vue
+++ src/pages/products.vue
@@ -104,7 +104,7 @@ const deleteProduct = async (id) => {
   getProducts();
 };
 
-// Edit productt
+// Edit producttt
 const editProduct = (product) => {
   editMode.value = true;
   editProductData.value = { ...product };
